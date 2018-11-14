d8e8f82bc670501b728a080b5a24e49904246fdb
Isuru Fernando <isuruf@gmail.com>
2018-11-14 15:41:55 -0600

    Workaround pari issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,11 @@ export CFLAGS="-g -fPIC $CFLAGS $CPPFLAGS"
 export CXXFLAGS="-g -fPIC $CXXFLAGS $CPPFLAGS"
 export SAGE_LOCAL="$PREFIX"
 
+# workaround for https://github.com/JohnCremona/eclib/issues/45
+if [ "$(uname)" == "Linux" ]; then
+    sed -i "s/long    rank(GEN x);//g" $PREFIX/include/pari/paridecl.h
+fi
+
 cd src
 make
 make install INSTALL_DIR="$PREFIX"
